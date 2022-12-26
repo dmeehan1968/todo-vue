f83e395ffaa5d29d7a8a9da4474f9b24fde083c5
Dave Meehan <dave_meehan@replicated.co.uk>
2022-12-26 16:50:05 +0000

    Add Amplify

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "todovue": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
