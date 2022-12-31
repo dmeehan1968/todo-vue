fc7f91c6ea8d09fbee30e404daca23988b7c1df9
Dave Meehan <dave_meehan@replicated.co.uk>
2022-12-31 12:29:23 +0000

    Add REST API

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -4,6 +4,40 @@ export type AmplifyDependentResourcesAttributes = {
             "GraphQLAPIKeyOutput": "string",
             "GraphQLAPIIdOutput": "string",
             "GraphQLAPIEndpointOutput": "string"
+        },
+        "todosApi": {
+            "RootUrl": "string",
+            "ApiName": "string",
+            "ApiId": "string"
+        }
+    },
+    "auth": {
+        "todovue": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "function": {
+        "todosLambda": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        }
+    },
+    "storage": {
+        "todosTable": {
+            "Name": "string",
+            "Arn": "string",
+            "StreamArn": "string",
+            "PartitionKeyName": "string",
+            "PartitionKeyType": "string",
+            "Region": "string"
         }
     }
 }
--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,53 @@
 <template>
-  <div v-if="pending">Loading...</div>
-  <ul v-else>
-    <li v-for="todo in todos">
-      <input type="checkbox" :checked="todo.completed" @change="todo.toggleCompleted()" /> {{ todo.name }}
-    </li>
-  </ul>
+  <Authenticator>
+    <template v-slot="{ user, signOut }">
+      <div class="welcome">
+        <h1>Hey {{ user.username }}</h1>
+        <button @click="signOut">Sign Out</button>
+      </div>
+      <h1>Todos</h1>
+      <div>
+        <button @click="getTodos">Get Todos</button>
+      </div>
+      <ul>
+        <li v-for="todo in todos">
+          {{ todo.name }}
+        </li>
+      </ul>
+    </template>
+  </Authenticator>
+<!--  <div v-if="pending">Loading...</div>-->
+<!--  <ul v-else>-->
+<!--    <li v-for="todo in todos">-->
+<!--      <input type="checkbox" :checked="todo.completed" @change="todo.toggleCompleted()" /> {{ todo.name }}-->
+<!--    </li>-->
+<!--  </ul>-->
 </template>
 
 <script setup lang="ts">
+import { Authenticator } from '@aws-amplify/ui-vue'
+import { ref } from "vue"
+import { API } from "aws-amplify"
 
-import { useTodos } from "@/composables/useTodos"
+function useTodos() {
+  const todos = ref([])
+  function getTodos() {
+    console.log('Get Todos')
+    API.get('todosApi', '/todos/x', {})
+        .then(res => console.log(res))
+        .catch(e => console.log(e))
+  }
 
-const { todos, pending } = useTodos()
+  return { todos, getTodos }
+}
+
+const { todos, getTodos } = useTodos()
+
+
+
+// import { useTodos } from "@/composables/useTodos"
+//
+// const { todos, pending } = useTodos()
 
 </script>
 
